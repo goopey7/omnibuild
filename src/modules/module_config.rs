3695d584aab23f789535aa75c61b7f40bc945e2f
Sam Collier <samcollier916@gmail.com>
2024-11-16 16:37:06 +0000

    start thinking about module cfg

---
+++ src/modules/module_config.rs
@@ -0,0 +1,33 @@
+enum ModuleType {
+    Dylib,
+    Lib,
+    Exe,
+}
+
+impl mlua::FromLua for ModuleType {
+    fn from_lua(value: mlua::Value, _: &mlua::Lua) -> mlua::Result<Self> {
+        match value {
+            mlua::Value::String(s) => match &*(s.to_str()?) {
+                "dylib" => Ok(ModuleType::Dylib),
+                "lib" => Ok(ModuleType::Lib),
+                "exe" => Ok(ModuleType::Exe),
+                other => Err(mlua::Error::FromLuaConversionError {
+                    from: "string",
+                    to: "ModuleType".to_string(),
+                    message: Some(format!("Invalid module_type: {}", other)),
+                }),
+            },
+            _ => Err(mlua::Error::FromLuaConversionError {
+                from: "non-string",
+                to: "ModuleType".to_string(),
+                message: Some("Expected a string".to_string()),
+            }),
+        }
+    }
+}
+
+pub struct ModuleConfig {
+    module_name: String,
+    module_type: ModuleType,
+    dependencies: Vec<String>,
+}
