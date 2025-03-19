5efb98c95749addf683bb348585cef792e2e31e6
Sam Collier <samcollier916@gmail.com>
2025-03-19 23:37:01 +0000

    example makefile (thanks claude)

--- example/src/server/ServerExecutable/main.cpp
+++ example/src/server/ServerExecutable/main.cpp
@@ -1,8 +1,10 @@
 #include <cstdint>
 #include <test.h>
+#include <print>
 
 int main()
 {
 	uint32_t i = test();
+	std::println("{}", i);
 	return 0;
 }
--- src/build/build.rs
+++ src/build/build.rs
@@ -1,9 +1,11 @@
-use crate::{build_state, cli::Cli};
+use crate::build::build_state::BUILD_STATE;
+use crate::cli::Cli;
 pub fn configure(lua: &mlua::Lua, args: &Cli) {
-    assert!(build_state!().project.is_some(), "project must be set!");
-    assert!(!build_state!().modules.is_empty(), "no modules have been added!");
-    assert!(!build_state!().targets.is_empty(), "no targets have been added!");
-    assert!(!build_state!().configs.is_empty(), "no configs have been added!");
+    let build_state = BUILD_STATE.try_read().expect("failed to get read on build_state");
+    assert!(build_state.project.is_some(), "project must be set!");
+    assert!(!build_state.modules.is_empty(),"no modules have been added!");
+    assert!(!build_state.targets.is_empty(),"no targets have been added!");
+    assert!(!build_state.configs.is_empty(),"no configs have been added!");
 
     // generate makefile, visual studio solution etc.
 }
